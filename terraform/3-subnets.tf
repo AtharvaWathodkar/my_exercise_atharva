b7d2407ad98a2bf1bd531298a26f0604e8b2476a
Atharva Wathodkar <atharva3990@gmail.com>
2022-08-17 11:55:09 +0530

    myterrfaorm

--- terraform/3-subnets.tf
+++ terraform/3-subnets.tf
@@ -3,7 +3,7 @@ resource "google_compute_subnetwork" "my_private" {
   name                     = "my_private"
   ip_cidr_range            = "10.0.0.0/18"
   region                   = "us-central1"
-  network                  = google_compute_network.main.id
+  network                  = google_compute_network.my_vpc.id
   private_ip_google_access = true
 
   secondary_ip_range {
